739df7d4795e44417dfca45ff61ca77d217bd6f8
maitretech <you@example.com>
2021-10-08 11:28:32 +0530

    logo and name added in url

--- components/layouts/Layout.js
+++ components/layouts/Layout.js
@@ -2,18 +2,26 @@ import Stickyfooter from '../stickyfooter/Stickyfooter'
 import Header from '../header/Header'
 import Footer from '../footer/Footer'
 import { ToastProvider } from 'react-toast-notifications/dist/ToastProvider'
+
+import Head from 'next/head'
 // import Headercontant from '../header/Headercontant'
 
-const Layout = ({children,data_header}) => {
+const Layout = ({ children, data_header }) => {
     return (
         <div>
-           <Header/>
-           {/* <Headercontant/> */}
-           {children}
-           <ToastProvider>
-            <Footer data_header={data_header}/>
+
+            <Head>
+                <title> Rukhmani Devi   Public School</title>
+                <link rel="icon" type="image/jpg" href="/images/pglogo.png" />
+            </Head>
+
+            <Header />
+            {/* <Headercontant/> */}
+            {children}
+            <ToastProvider>
+                <Footer data_header={data_header} />
             </ToastProvider>
-           {/* <Stickyfooter/> */}
+            {/* <Stickyfooter/> */}
 
         </div>
     )
