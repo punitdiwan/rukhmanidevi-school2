1cba4f2f8e77551ecc7f1a5399b0fe1f32a3e749
shivam sharma <shivam>
2021-10-21 15:52:58 +0530

    fees pdf added

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a4f16b263d3a47a31922";
+/******/ 	var hotCurrentHash = "97a0adc88b8c7ec2dfc6";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
