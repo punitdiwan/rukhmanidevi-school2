a4c7aa2153660e2cadb5d0eae9e62030f0dd91da
shivam sharma <shivam>
2021-10-26 11:38:45 +0530

    location address change done

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "02669e960ee4a49b1a92";
+/******/ 	var hotCurrentHash = "5b533ccbfac01ddacd4d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/Location.js
+++ pages/Location.js
@@ -139,19 +139,19 @@ const Location = () => {
                                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" style={{ width: "16px", color: "#0a7ad1" }}>
                                             <path fill-rule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd" />
                                         </svg>&nbsp;&nbsp;
-                                        <span> Kuthar Road, Subathu, District Solan HP, 173 206 (India )</span>
+                                        <span> Airport Road,Lalghati</span>
                                     </div>
                                     <div className="mt-1">
                                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" style={{ width: "18px", color: "#0a7ad1" }}>
                                             <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
                                         </svg>&nbsp;&nbsp;
-                                        <span>91-1792-275690 , 91-1792-275691</span>
+                                        <span>  9993392708 8109048886</span>
                                     </div>
                                     <div className="mt-1">
                                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" style={{ width: "18px", color: "#0a7ad1" }}>
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 18h.01M8 21h8a2 2 0 002-2V5a2 2 0 00-2-2H8a2 2 0 00-2 2v14a2 2 0 002 2z" />
                                         </svg>&nbsp;&nbsp;
-                                        <span>91-1792-275666</span>
+                                        <span>8109048886</span>
                                     </div>
 
                                     <div className="mt-1">
